5ad8ee860af92ecdac556290af8a10410c813de5
Anthony Wong <awong0811@gmail.com>
2024-10-01 14:02:02 -0700

    updated switch example

--- arduino-examples/switch/switch.ino
+++ arduino-examples/switch/switch.ino
@@ -2,7 +2,7 @@ const int switchPin = 2; // Pin connected to the NO switch
 const char outputChar = 'A'; // Character to output when switch is pressed
 
 void setup() {
-  pinMode(switchPin, INPUT); // Set the switch pin as an INPUT
+  pinMode(switchPin, INPUT_PULLUP); // Set the switch pin as an INPUT
   Serial.begin(9600); // Start the Serial communication at 9600 baud
 }
 
@@ -11,8 +11,8 @@ void loop() {
   int switchState = digitalRead(switchPin);
   
   // Check if the switch is pressed
-  if (switchState == HIGH) {
+  if (switchState == LOW) {
     Serial.println(outputChar); // Output the character to Serial Monitor
-    delay(500); // Delay to avoid multiple readings from one press
+    delay(100); // Delay to avoid multiple readings from one press
   }
 }
