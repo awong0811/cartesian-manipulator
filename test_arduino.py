a49af95e6511a095a46ccff59204db918e9ed236
Anthony Wong <awong0811@gmail.com>
2024-10-22 15:43:14 -0700

    tested distance and wait times for each distance

--- test_arduino.py
+++ test_arduino.py
@@ -13,62 +13,39 @@ time.sleep(0.5)
 response = instrument.read_response()
 print(response)
 
+wait_times = {
+    1: 2.5,
+    2: 3.5,
+    3: 4,
+    4: 4.5,
+    5: 5,
+    6: 5.5,
+    7: 6,
+    8: 6.5,
+    9: 7,
+    10: 7.5,
+    11: 7.5
+}
 
-# instrument.send_command(command='X1+1000')
-# while True:
-#     instrument.send_command(command='X1#')
-#     time.sleep(0.1)
-#     response = instrument.read_response()
-#     if response and int(response[3:])==0:
-#         break
-#     time.sleep(1)
-# instrument.send_command(command='X2+1000')
-# while True:
-#     instrument.send_command(command='X2#')
-#     time.sleep(0.1)
-#     response = instrument.read_response()
-#     if response and int(response[3:])==0:
-#         break
-#     time.sleep(1)
-instrument.send_command(command='X2-10000')
-time.sleep(10)
-# while True:
-#     instrument.send_command(command='X2#')
-#     time.sleep(1)
-#     response = instrument.read_response()
-#     print(int(response[3:]))
-#     if response and int(response[3:])==0:
-#         print("X1 has stopped")
-#         break
-#     time.sleep(1)
-# instrument.send_command(command='X1-10000')
-# while True:
-#     instrument.send_command(command='X1#')
-#     time.sleep(0.1)
-#     response = instrument.read_response()
-#     if response and int(response[3:])==0:
-#         break
-#     time.sleep(1)
-# instrument.send_command(command='X1+10000,X2+10000')
-# while True:
-#     instrument.send_command(command='X1#')
-#     time.sleep(0.1)
-#     response1 = instrument.read_response()
-#     instrument.send_command(command='X2#')
-#     time.sleep(0.1)
-#     response2 = instrument.read_response()
-#     if response1 and response2 and response1[]==0 and response2==0:
-#         break
-#     time.sleep(1)
 # instrument.send_command(command='X1-10000,X2-10000')
-# while True:
-#     instrument.send_command(command='X1#')
-#     time.sleep(0.1)
-#     response1 = instrument.read_response()
-#     instrument.send_command(command='X2#')
-#     time.sleep(0.1)
-#     response2 = instrument.read_response()
-#     if response1 and response2 and response1==0 and response2==0:
-#         break
-#     time.sleep(1)
+# time.sleep(10)
+
+# dist = 9999
+# instrument.send_command(command=f'X2-{dist}')
+# time.sleep(wait_times[dist//1000+1])
+dist = 4999
+instrument.send_command(command=f'X2+{dist}')
+time.sleep(wait_times[dist//1000+1])
+instrument.send_command(command=f'X2+{dist}')
+time.sleep(wait_times[dist//1000+1])
+
+# dist = 20000
+# instrument.send_command(command=f'X2+{dist}')
+# time.sleep(15)
+# dist = 19000
+# instrument.send_command(command=f'X2-{dist}')
+# time.sleep(15)
+
+# 19000 is the max distance from the switch!!!
+
 instrument.disconnect()
