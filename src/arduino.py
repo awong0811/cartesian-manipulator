bb9e726fc1ce5cd4d38b8a1a139809ede863d487
Anthony Wong <awong0811@gmail.com>
2024-11-19 17:41:30 -0800

    updated reset and move functions

--- src/arduino.py
+++ src/arduino.py
@@ -63,7 +63,7 @@ class Arduino():
         Checks if the response is a switch press.
         '''
         data = self.read_response()
-        if len(data)==2 and data[0]=='S' and data[1].isnumeric():
+        if data and len(data)==2 and data[0]=='S' and data[1].isnumeric():
             return True, int(data[1])
         else:
             return False, 0
@@ -71,15 +71,31 @@ class Arduino():
     def reset(self, motor: List):
         assert(len(motor)<=4)
         assert(max(motor)<=4)
-        dist = []
-        for i in range(len(motor)):
-            dist.append(-19000)
-        self.move(motor=motor,dist=dist)
-        self.motor1_coord = 0
-        self.motor2_coord = 0
-        self.motor3_coord = 0
-        self.motor4_coord = 0
-        return
+        assert(min(motor)>=0)
+        ### RESET ALL MOTORS TOGETHER ###
+        # dist = []
+        # for i in range(len(motor)):
+        #     dist.append(-19000)
+        # self.move(motor=motor,dist=dist)
+        #################################
+
+        ### RESET EACH MOTOR ONE BY ONE ###
+        for x in motor:
+            motor_name = config.MOTOR_NAME[x]
+            command = motor_name+'+19000'
+            self.send_command(command=command)
+            while True:
+                # pressed, switch_num = self.check_switch()
+                # if pressed:
+                #     print(f"Switch {switch_num} pressed!")
+                #     break
+                response = self.read_response()
+                if response:
+                    print(response)
+                    break
+        ###################################
+        print("All motors reset!")
+        return None
 
 
     def move(self, motor: List, dist: List):
@@ -105,7 +121,8 @@ class Arduino():
                 sign = '+'
             else:
                 sign = '-'
-            command = command + f',X{motor[i]}'+sign+f'{abs(distance)}'
+            command = command + ',' + config.MOTOR_NAME[motor[i]] + sign + f'{abs(distance)}'
+            # command = command + f',X{motor[i]}'+sign+f'{abs(distance)}'
             # save the position of each motor
             setattr(self, f'motor{i}', destination)
         self.send_command(command=command[1:])
@@ -120,5 +137,4 @@ class Arduino():
             coord = getattr(self, f'motor{i}')
             coords.append(coord)
             print(f"Motor {i}: {coord}")
-        return coord
-        
+        return coords
--- test_arduino.py
+++ test_arduino.py
@@ -13,26 +13,11 @@ time.sleep(0.5)
 response = instrument.read_response()
 print(response)
 
-wait_times = {
-    1: 2.5,
-    2: 3.5,
-    3: 4,
-    4: 4.5,
-    5: 5,
-    6: 5.5,
-    7: 6,
-    8: 6.5,
-    9: 7,
-    10: 7.5,
-    11: 7.5
-}
-
-
 instrument.reset([1])
 instrument.reset([2])
-for i in range(10):
-    instrument.move([1],[1000])
-    instrument.move([2],[1000])
-
+instrument.get_coords()
+# for i in range(10):
+#     instrument.move([1],[1000])
+#     instrument.move([2],[1000])
 
 instrument.disconnect()
